8e950f467b114ab1fb8c168c18a695925eba3eb4
sakh1 <sakh1alkhimova@gmail.com>
2023-09-07 18:25:58 +0300

    вариант_2

--- src/Lessen6.java
+++ src/Lessen6.java
@@ -40,5 +40,22 @@ public class Lessen6 {
         System.out.println("c - d = " + (c - d));
         System.out.println("c * d = " + (c* d));
         System.out.println("c / d = " + (c / d));
+
+        // Проверка перегрузки
+        byte e = -128;
+        e -= 2;
+        System.out.println("Перегрузка byte: " + e);
+        short f = -32768;
+        f += 5;
+        System.out.println("Перегрузка short: " + f);
+        int g = 123456;
+        g -= 10;
+        System.out.println("Перегрузка int: " + g);
+        long h = 1234455768;
+        h -= 15;
+        System.out.println("Перегрузка long: " + h);
+        char i = 'a';
+        i +=1244;
+        System.out.println("Перегрузка char: " + i);
     }
 }
